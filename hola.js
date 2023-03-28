bdda375e6c7e70c1c19be61c8e0fcba9824b8329
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2023-03-28 15:06:43 -0600

    Programa inicial hola mundo typescript

---
+++ hola.js
@@ -0,0 +1,4 @@
+function saluda(nombre) {
+    return "\u00A1Hola buenos dias, ".concat(nombre.toUpperCase(), "!");
+}
+console.log(saluda("Dani"));
---
+++ hola.ts
@@ -0,0 +1,5 @@
+function saluda(nombre: string): string {
+    return `¡Hola buenos dias, ${nombre.toUpperCase()}!`
+}
+
+console.log(saluda("Dani"));
