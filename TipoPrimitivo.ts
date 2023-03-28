b78d35a706c5097b136d6364ae6e0090c7c757c0
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2023-03-28 15:22:29 -0600

    Tipos de datos primitivos

---
+++ TipoPrimitivo.ts
@@ -0,0 +1,21 @@
+/**
+ * Boolean
+ */
+let logico: boolean = false;
+let verdadero: boolean = true;
+
+/**
+ * Number
+ */
+let entero: number = 44;
+let decimal: number = 26.05;
+let hexadecimales: number = 0x12345
+let octales: number = 0o700
+let binario: number = 0b1010101
+
+let extremadamentelargo: number = 7_812_341_121
+
+/**
+ * strings
+ */
+let cadenas: string = "dfasfas1233654|";
